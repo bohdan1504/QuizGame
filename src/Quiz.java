0ff6c89ad9d95a69e4888198ff6691fd280be60b
Bogdan <bandrashko1@gmail.com>
2022-07-30 20:00:08 +0200

    Added comments

--- src/Quiz.java
+++ src/Quiz.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 
 public class Quiz extends JFrame implements ActionListener {
 
+    // array with questions
     String[] questions = {
             "Which company created Java?",
             "Which year was Java created?",
@@ -12,6 +13,7 @@ public class Quiz extends JFrame implements ActionListener {
             "Who is credited with creating Java?"
     };
 
+    // 2D array with options
     String[][] options = {
             {"Sun Microsystems", "Starbucks", "Microsoft", "Alphabet"},
             {"1989", "1996", "1972", "1496"},
@@ -19,6 +21,7 @@ public class Quiz extends JFrame implements ActionListener {
             {"Steve Jobs", "Bill Gates", "James Gosling", "Mark Zuckerberg"}
     };
 
+    // array of correct answers
     char[] answers = {
             'A',
             'B',
@@ -26,12 +29,11 @@ public class Quiz extends JFrame implements ActionListener {
             'C'
     };
 
+    // global colors
     Color bgColor = new Color(25,25,25);
     Color fontColor = new Color(25,255,0);
     Color wrongAnswerColor = new Color(255,0,0);
 
-
-//    char guess;
     char answer;
     int index;
     int correct_guesses = 0;
@@ -39,6 +41,7 @@ public class Quiz extends JFrame implements ActionListener {
     int result;
     int seconds=10;
 
+    // GUI components
     JTextField textField = new JTextField();
     JTextArea textArea = new JTextArea();
     JButton buttonA = new JButton();
@@ -55,6 +58,7 @@ public class Quiz extends JFrame implements ActionListener {
     JTextField percentage = new JTextField();
     JButton retry = new JButton("Try again");
 
+    // countdown timer in the bottom right corner
     Timer countdown = new Timer(1000, e -> {
         seconds--;
         seconds_left.setText(String.valueOf(seconds));
@@ -63,8 +67,10 @@ public class Quiz extends JFrame implements ActionListener {
         }
     });
 
+    // main body
     Quiz(){
 
+        // setting up the frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Quiz Game");
         this.setSize(650,650);
@@ -73,6 +79,7 @@ public class Quiz extends JFrame implements ActionListener {
         this.setLocationRelativeTo(null);
         this.setResizable(false);
 
+        // top text field: used for Title and displays "Results" in the end
         textField.setBounds(0,0,650,50);
         textField.setBackground(bgColor);
         textField.setForeground(fontColor);
@@ -82,6 +89,7 @@ public class Quiz extends JFrame implements ActionListener {
         textField.setEditable(false);
         textField.setText("Quiz Game");
 
+        // displays a question
         textArea.setBounds(0,50,650,50);
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
@@ -92,50 +100,60 @@ public class Quiz extends JFrame implements ActionListener {
         textArea.setEditable(false);
         textArea.setText("Sample text?");
 
+        // button A
         buttonA.setBounds(0,100,100,100);
         buttonA.setFont(new Font("MV Boli",Font.BOLD,35));
         buttonA.setFocusable(false);
         buttonA.addActionListener(this);
         buttonA.setText("A");
 
+        // button B
         buttonB.setBounds(0,200,100,100);
         buttonB.setFont(new Font("MV Boli",Font.BOLD,35));
         buttonB.setFocusable(false);
         buttonB.addActionListener(this);
         buttonB.setText("B");
 
+        // button C
         buttonC.setBounds(0,300,100,100);
         buttonC.setFont(new Font("MV Boli",Font.BOLD,35));
         buttonC.setFocusable(false);
         buttonC.addActionListener(this);
         buttonC.setText("C");
 
+        // button D
         buttonD.setBounds(0,400,100,100);
         buttonD.setFont(new Font("MV Boli",Font.BOLD,35));
         buttonD.setFocusable(false);
         buttonD.addActionListener(this);
         buttonD.setText("D");
 
+        // answer A
         answer_labelA.setBounds(125,100,500,100);
         answer_labelA.setBackground(bgColor);
         answer_labelA.setForeground(fontColor);
         answer_labelA.setFont(new Font("MV Boli",Font.PLAIN,35));
 
+        // answer B
         answer_labelB.setBounds(125,200,500,100);
         answer_labelB.setBackground(bgColor);
         answer_labelB.setForeground(fontColor);
         answer_labelB.setFont(new Font("MV Boli",Font.PLAIN,35));
 
+        // answer C
         answer_labelC.setBounds(125,300,500,100);
         answer_labelC.setBackground(bgColor);
         answer_labelC.setForeground(fontColor);
         answer_labelC.setFont(new Font("MV Boli",Font.PLAIN,35));
 
+        // answer D
         answer_labelD.setBounds(125,400,500,100);
         answer_labelD.setBackground(bgColor);
         answer_labelD.setForeground(fontColor);
         answer_labelD.setFont(new Font("MV Boli",Font.PLAIN,35));
 
+        // displays countdown in the right bottom corner,
+        // updates every second with the timer set up outside the Quiz body
         seconds_left.setBounds(535,510,100,100);
         seconds_left.setBackground(bgColor);
         seconds_left.setForeground(new Color(255,0,0));
@@ -145,6 +163,7 @@ public class Quiz extends JFrame implements ActionListener {
         seconds_left.setHorizontalAlignment(JTextField.CENTER);
         seconds_left.setText(String.valueOf(seconds));
 
+        // "timer" label for the countdown
         time_label.setBounds(535,475,100,25);
         time_label.setBackground(bgColor);
         time_label.setForeground(new Color(255,0,0));
@@ -152,6 +171,7 @@ public class Quiz extends JFrame implements ActionListener {
         time_label.setHorizontalAlignment(JTextField.CENTER);
         time_label.setText("timer >:D");
 
+        // displays number of correct guesses in the end of the Quiz
         number_right.setBounds(225,225,200,100);
         number_right.setBackground(bgColor);
         number_right.setForeground(fontColor);
@@ -160,6 +180,7 @@ public class Quiz extends JFrame implements ActionListener {
         number_right.setHorizontalAlignment(JTextField.CENTER);
         number_right.setEditable(false);
 
+        // displays percentage of correct guesses in the end of the Quiz
         percentage.setBounds(225,325,200,100);
         percentage.setBackground(bgColor);
         percentage.setForeground(fontColor);
@@ -168,6 +189,7 @@ public class Quiz extends JFrame implements ActionListener {
         percentage.setHorizontalAlignment(JTextField.CENTER);
         percentage.setEditable(false);
 
+        // displays "Try again" button in the end of the Quiz
         retry.setBounds(225,425,200,50);
         retry.setOpaque(true);
         retry.setFocusable(false);
@@ -194,9 +216,7 @@ public class Quiz extends JFrame implements ActionListener {
 
     public void nextQuestion(){
 
-//        seconds=10;
-//        seconds_left.setText(String.valueOf(seconds));
-
+        // main function to display content: the question and answers
         if (index>=total_questions){
             results();
         } else {
@@ -206,11 +226,12 @@ public class Quiz extends JFrame implements ActionListener {
             answer_labelB.setText(options[index][1]);
             answer_labelC.setText(options[index][2]);
             answer_labelD.setText(options[index][3]);
-            countdown.start();
+            countdown.start(); // starting the countdown in the bottom right corner
         }
 
     }
 
+    // highlights wrong answers after users choice
     public void displayAnswer(){
 
         countdown.stop();
@@ -230,12 +251,12 @@ public class Quiz extends JFrame implements ActionListener {
             answer_labelD.setForeground(wrongAnswerColor);
         }
 
+        // 2 seconds pause before the next question
         Timer pause = new Timer(2000, e -> {
                 answer_labelA.setForeground(fontColor);
                 answer_labelB.setForeground(fontColor);
                 answer_labelC.setForeground(fontColor);
                 answer_labelD.setForeground(fontColor);
-
                 answer = ' ';
                 seconds = 10;
                 seconds_left.setText(String.valueOf(seconds));
@@ -252,6 +273,7 @@ public class Quiz extends JFrame implements ActionListener {
 
     }
 
+    // display results after last question
     public void results(){
         buttonA.setEnabled(false);
         buttonB.setEnabled(false);
@@ -274,6 +296,7 @@ public class Quiz extends JFrame implements ActionListener {
         retry.addActionListener(e -> newGame());
     }
 
+    // resets everything and starts from question 1 again
     public void newGame(){
         buttonA.setEnabled(true);
         buttonB.setEnabled(true);
